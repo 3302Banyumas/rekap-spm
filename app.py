1984be93916bb6546699fef7117222d00ebf4c34
3302Banyumas <bps.banyumas@gmail.com>
2025-08-21 15:18:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ sheet_url = "https://docs.google.com/spreadsheets/d/1WJR1tCiYRCJYbvfcW8fMySuqzG5
 df = pd.read_csv(sheet_url)
 
 # --- FITUR PENCARIAN ---
-search = st.text_input("Cari Data:", placeholder="Ketik nomor SPM/nomor FP")
+search = st.text_input("Cari Data:", placeholder="Ketik judul pencairan/nomor SPM/nomor FP")
 
 if search:
     df_filtered = df[df.astype(str).apply(lambda x: x.str.contains(search, case=False)).any(axis=1)]
@@ -22,4 +22,5 @@ else:
 
 # --- TAMPILKAN TABEL ---
 st.dataframe(df_filtered, use_container_width=True)
+
 
